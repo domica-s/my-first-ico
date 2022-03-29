82e77abaf42b07afb809eb6d08752a8e92fe6685
Domica <1155130940@link.cuhk.edu.hk>
2022-03-29 21:39:08 +0800

    second commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc072446aC95fd3B8fEb2a633cCB2A54ffa1Eac89";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
